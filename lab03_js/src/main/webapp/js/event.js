ed567dc598b6896f6c88b17d368506f1886d891f
LIMGEONHEE <dlarjsgml321@naver.com>
2024-05-20 18:46:37 +0900

    5/20

--- lab03_js/src/main/webapp/js/event.js
+++ lab03_js/src/main/webapp/js/event.js
@@ -1,10 +1,53 @@
 /**
- * event.html에 포함
+ * event.html에 포함.
  */
 
- const output = document.querySelector('div#output');
- 
- const btnInput = document.querySelector('button#btnInput');
- btnInput.addEventListener('click', function (e) {
-    console.log(e);    
- });
+// button#btnInput 요소를 찾음:
+const btnInput = document.querySelector('button#btnInput');
+
+// btnInput에 클릭 이벤트 리스너를 설정:
+// input#itemInput에 입력된 내용을 ul 요소의 li로 추가.
+btnInput.addEventListener('click', function (e) {
+//    console.log(e); //-> PointerEvent
+    
+    const itemInput = document.querySelector('input#itemInput');
+    const itemList = document.querySelector('ul#itemList');
+    itemList.innerHTML += `<li> ${itemInput.value} </li>`;
+    itemInput.value = '';
+});
+
+// TODO: input#itemInput2 요소에 'keydown' 이벤트 리스너를 등록:
+// 엔터키가 눌렸을 때, input에 입력된 내용을 ul#itemList2의 리스트 아이템으로 추가.
+
+document.getElementById('itemInput2').addEventListener('keydown', (e) => {
+    if (e.key == 'Enter') {
+        const itemInput2 = document.querySelector('input#itemInput2');
+        const itemList2 = document.querySelector('ul#itemList2');
+        itemList2.innerHTML += `<li> ${itemInput2.value} </li>`;
+        itemInput2.value = '';
+    }
+});
+
+// TODO: input#username 요소에 'change' 이벤트 리스너를 등록:
+// input에 입력된 내용이 바뀔 때마다 div를 입력 내용으로 덮어씀.
+document.getElementById('username').addEventListener('change', (e) => {
+    const username = document.querySelector('#username');
+    const output = document.querySelector('#output');
+    output.innerHTML = `<div> ${username.value} </div>`;
+});
+
+// TODO: img#bulb 요소에 'mouseenter' 이벤트 리스너를 등록:
+// img의 src를 'images/bulb_on.gif'로 변경.
+document.querySelector('#bulb').addEventListener('mouseenter', (e) => {
+    const bulb = document.querySelector('#bulb');
+    bulb.src = 'images/bulb_on.gif';
+    
+});
+
+// TODO: img#bulb 요소에 'mouseleave' 이벤트 리스너를 등록:
+// img의 src를 'images/bulb_off.gif'로 변경.
+document.querySelector('#bulb').addEventListener('mouseleave', (e) => {
+    const bulb = document.querySelector('#bulb');
+    bulb.src = 'images/bulb_off.gif';
+    
+});
