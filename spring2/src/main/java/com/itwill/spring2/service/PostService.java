ce8db5d53097283dd9163b3e6514d4e6512cca97
LIMGEONHEE <dlarjsgml321@naver.com>
2024-06-11 15:20:39 +0900

    6/11

--- spring2/src/main/java/com/itwill/spring2/service/PostService.java
+++ spring2/src/main/java/com/itwill/spring2/service/PostService.java
@@ -2,7 +2,6 @@ package com.itwill.spring2.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.itwill.spring2.repository.Post;
@@ -15,21 +14,21 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor // final 필드들을 초기화하는 아규먼트를 갖는 생성자.
 @Service // 스프링 컨테이너에 서비스 컴포넌트로 등록.
 public class PostService {
+    
+    // 애너테이션을 사용한 의존성 주입(DI: Dependency Injection):
+    // @Autowired private PostDao postDao;
+    
+    // 생성자에 의한 의존성 주입:
+    // (1) final 필드 선언. (2) final 필드를 초기화하는 생성자 작성.
+    private final PostDao postDao; 
+//    public PostService(PostDao postDao) {
+//        this.postDao = postDao;
+//    }
 
-// 애너테이션을 사용한 의존성 주입(DI: Dependency Injection):
-//	@Autowired public PostDao postDao;
-	
-	// 생성자에 의한 의존성 주입:
-	// (1) final 필드 선언. (2) final 필드를 초기화하는 생성자 작성.
-	private final PostDao postDao;
-//	public PostService() {
-//		this.postDao = postDao;
-//	}
-	
-	public List<Post> read() {
-		log.debug("read()");
-		
-		return postDao.selectOrderByIdDesc();
-	}
-	
+    public List<Post> read() {
+        log.debug("read()");
+        
+        return postDao.selectOrderByIdDesc();
+    }
+    
 }
--- spring2/src/main/java/com/itwill/spring2/web/PostController.java
+++ spring2/src/main/java/com/itwill/spring2/web/PostController.java
@@ -19,18 +19,17 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/post")
 //-> PostController 클래스의 모든 컨트롤러 메서드의 매핑 주소는 "/post"로 시작.
 public class PostController {
+    
+    private final PostService postService; // 생성자에 의한 의존성 주입
 
-	private final PostService postService; // 생성자에 의한 의존성 주입 
-	
     @GetMapping("/list")
     public void list(Model model) {
-    		   	
         log.debug("list()");
         
         // 서비스 컴포넌트의 메서드를 호출, 포스트 목록을 읽어옴 -> 뷰에 전달.
         List<Post> list = postService.read();
         model.addAttribute("posts", list);
-        		
+        
         // 뷰: /WEB-INF/views/post/list.jsp
     }
     
