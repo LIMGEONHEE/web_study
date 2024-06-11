801605211f11aee44bfc2801dbb2b73d24dfc06b
LIMGEONHEE <dlarjsgml321@naver.com>
2024-06-11 15:18:17 +0900

    6/11

--- spring2/src/test/java/com/itwill/spring2/repository/PostDaoTest.java
+++ spring2/src/test/java/com/itwill/spring2/repository/PostDaoTest.java
@@ -18,17 +18,56 @@ import lombok.extern.slf4j.Slf4j;
 
 public class PostDaoTest {
 
-	@Autowired
+	@Autowired // 의존성 주입(DI)
 	private PostDao postDao;	
 	
-	@Test
+//	@Test
 	public void testSelectAll() {
 		Assertions.assertNotNull(postDao);
 		
 		List<Post> list = postDao.selectOrderByIdDesc();
 		for (Post p : list) {
-			System.out.println(p);
+			System.out.println("\t" + p);
 		}
 		
 	}
+	
+//	@Test
+	public void testselectById() {
+		Post post1 = postDao.selectById(4); // DB 테이블에 id가 있는 경우
+		Assertions.assertNotNull(post1);
+		log.debug(post1.toString());
+		
+		Post post2 = postDao.selectById(1); // DB 테이블에 id가 없는 경우
+		Assertions.assertNotNull(post2);
+	}
+	
+//	@Test
+	public void testInsert() {
+		// insert할 데이터
+		Post post = Post.builder().title("MyBatis 테스트")
+							.content("MyBatis-Spring insert 테스트")
+							.author("admin")
+							.build();
+		int result = postDao.insertPost(post);
+		Assertions.assertEquals(1, result);
+	}
+	
+//	@Test
+	public void testUpdate() {
+		// 업데이트할 포스트 객체:
+		Post post = Post.builder().id(14)
+								.title("MyBatis 업데이트")
+								.content("MyBatis-Spring을 사용한 데이터베이스 테이블 업데이트")
+								.build();
+		int result = postDao.updatePost(post);
+		Assertions.assertEquals(1, result);
+	}
+	
+	@Test
+	public void testDelete() {
+		int result = postDao.deletePost(27);
+		Assertions.assertEquals(1, result);
+	}
+	
 }
