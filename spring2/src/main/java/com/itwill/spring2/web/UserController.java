15422134f762ddcdbf2727d9a97b8b680815a8fa
LIMGEONHEE <dlarjsgml321@naver.com>
2024-06-19 11:14:35 +0900

    6/19

--- spring2/src/main/java/com/itwill/spring2/web/UserController.java
+++ spring2/src/main/java/com/itwill/spring2/web/UserController.java
@@ -1,21 +1,41 @@
 package com.itwill.spring2.web;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.itwill.spring2.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/user")
 public class UserController {
     
+	private final UserService userService;
+	
     @GetMapping("/signup") // GET 방식의 /user/signup 요청을 처리하는 컨트롤러 메서드 
     public void signup() {
         log.debug("GET signup()");
     }
     
-    // TODO: 사용자 아이디 중복체크 REST 컨트롤러
-
+    // 사용자 아이디 중복체크 REST 컨트롤러
+    @GetMapping("/checkid")
+    @ResponseBody // 메서드 리턴 값이 클라이언트로 전달되는 데이터, RestController에서는 사용하지 않는다.
+    public ResponseEntity<String> checkId(@RequestParam(name = "userid") String userid) {
+    	log.debug("checkId(userid={})", userid);
+    	
+    	boolean result = userService.checkUserid(userid);
+    	if (result) {
+    		return ResponseEntity.ok("Y");
+    	} else {
+    		return ResponseEntity.ok("N");
+    	}
+    }
 }
